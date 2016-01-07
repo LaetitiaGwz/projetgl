19555aea244948698a1b2655f5d243964f183f18
Florentin Gonthier <florentin.gonthier@phelma.grenoble-inp.fr>
2016-01-07 16:42:23 +0100

    diplayUsage complete parseArgs en cours

--- src/main/java/fr/ensimag/deca/CompilerOptions.java
+++ src/main/java/fr/ensimag/deca/CompilerOptions.java
@@ -43,7 +43,14 @@ public class CompilerOptions {
 
     
     public void parseArgs(String[] args) throws CLIException {
-        // A FAIRE : parcourir args pour positionner les options correctement.
+        // A FAIRE : parcourir args pour positionner les options correctement
+		if (args.length==1){
+			throw new CLIException("Aucun parametre rentree");
+		}
+		if(args.length==2 && args[1].equals("-b")){
+			this.printBanner=true;
+		}
+
         Logger logger = Logger.getRootLogger();
         // map command-line debug option to log4j's level.
         switch (getDebug()) {
@@ -67,10 +74,20 @@ public class CompilerOptions {
             logger.info("Java assertions disabled");
         }
 
-        throw new UnsupportedOperationException("not yet implemented");
+        throw new CLIException("commande incorrect");
     }
 
     protected void displayUsage() {
-        throw new UnsupportedOperationException("not yet implemented");
-    }
+        System.out.println("Voici les différents fonctions du compilateur:");
+		System.out.println("-b affiche le nom de l'equipe, doit etre utiliser seul");
+		System.out.print("-p apllique le parser et décompile celui-ci et l'affiche, doit etre suivi au moins d'un nom de fichier source ");
+		System.out.println(" ne peux etre utilisé avec -v");
+    	System.out.print("-v arrete decac après la vérification, ne produit pas de sortie, doit etre suivi au moins d'un nom de fichier source");
+		System.out.print("ne peut estre suivit de -p");
+		System.out.println("-n enleve les tests de débordements (arithméthique,mémoire,null");
+		System.out.println("-r X limite l'utilisation des registres à R(X-1), nb: 4 <= X <= 16");
+		System.out.println("-d active les traces de debug, activer plusieurs fois pour avoir plus de traces");
+		System.out.println("-P si plusieurs fichiers sources lance la compilation en parallèle");
+		System.out.println("-w affiche les warnings");
+	}
 }
