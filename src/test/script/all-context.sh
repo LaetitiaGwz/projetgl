d8eb64be4c7ee0b13595e47cd7ec7a60010ff1ea
Matthieu Bergeron <matthieu.bergeron@ensimag.grenoble-inp.fr>
2016-01-11 22:28:02 +0100

    **Ajout d'un script pour tester automatiquement la vérification contextuelle. Ce script permet aussi de savoir quelles fonctionnalités sont : - Encore a implémenter - Incorrectes (à corriger) -Correctes
    **correction du test decl_void

---
+++ src/test/script/all-context.sh
@@ -0,0 +1,58 @@
+#! /bin/bash
+
+# Auteur : matthieu
+# Script d'automatisation des tests pour la verification contextuelle
+
+# On se place dans le répertoire du projet (quel que soit le
+# répertoire d'où est lancé le script) :
+cd "$(dirname "$0")"/../../.. || exit 1
+
+PATH=./src/test/script/launchers:"$PATH"
+INVALID_DIR=src/test/deca/etapeB/invalid
+VALID_DIR=src/test/deca/etapeB/valid
+
+GREEN="\e[1;32m"
+WHITE="\e[1;37m"
+RED="\e[31m"
+YELLOW="\e[33m"
+
+return_status=0
+
+# Tests des cas invalides
+for cas_de_test in "$INVALID_DIR"/*.deca
+do
+    # Récupération de la ligne ou se produit l'erreur, à partir des commentaires
+    # du fichier de test
+    line_err=$(cat ${cas_de_test} | grep Ligne | sed -e "s/[^0-9]//g")
+    filename=$(echo ${cas_de_test} | sed -e "s@${INVALID_DIR}/@@g")
+    result_test=$(test_context "$cas_de_test" 2>&1)
+    if  echo "$result_test" | grep -q "$filename"':[0-9]'
+    then
+        echo -e "$filename"" : ${GREEN}  OK ${WHITE}"
+    elif echo "$result_test" | grep -q "UnsupportedOperationException"
+    then
+        echo -e "$filename"" : ${YELLOW}  NOT IMPLEMENTED ${WHITE}"
+    else
+        echo -e "$filename"" : ${RED}  ERROR ${WHITE}"
+        return_status=1
+    fi
+done
+
+# Tests des cas valides
+for cas_de_test in "$VALID_DIR"/*.deca
+do
+    filename=$(echo ${cas_de_test} | sed -e "s@${VALID_DIR}/@@g")
+    result_test=$(test_context "$cas_de_test" 2>&1)
+    if  echo "$result_test" | grep -q "$filename"':[0-9]'
+    then
+        echo -e "$filename"" : ${RED}  ERROR ${WHITE}"
+    elif echo "$result_test" | grep -q "UnsupportedOperationException"
+    then
+        echo -e "$filename"" : ${YELLOW}  NOT IMPLEMENTED ${WHITE}"
+    else
+        echo -e "$filename"" : ${GREEN}  OK ${WHITE}"
+        return_status=1
+    fi
+done
+
+exit ${return_status}
