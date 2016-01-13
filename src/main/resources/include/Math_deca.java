0f1e572666bfa12ff1fae494179c6250e9501e36
Fischman <fischman.adrien@gmail.com>
2016-01-13 20:13:20 +0100

    ajout de Math_deca.sqrt

--- src/main/resources/include/Math_deca.java
+++ src/main/resources/include/Math_deca.java
@@ -2,6 +2,8 @@ import java.lang.Math ;
 
 public class Math_deca{
 	public static final float MIN_VALUE = (float)1.4E-45;
+	public static final float MAX_VALUE = (float)1.633123935319537E16;
+	public static final float PI = (float)3.141592653589793;
 	public static float power(float x, int y){
 
 		float originalX = x ;
@@ -47,6 +49,99 @@ public class Math_deca{
 		}
 		return power(2,exp - 23) ;	
 	}
+	// Méthode de Cordic cf :http://www.trigofacile.com/maths/trigo/calcul/cordic/cordic.htm
+	public static float tan(float theta){
 
+		// On se ramène à des angles entre -PI/2 et PI/2
+		while ( theta > PI/2.0 || theta < -PI/2.0){
+			theta = (theta < 0) ? theta + PI : theta - PI ;
+		}
+
+		// On se ramène à des angles positifs
+		int sign = 1;
+		if ( theta < 0) {
+			theta = - theta;
+			sign = -1;
+		}
+
+		if (theta == Math_deca.PI/2.0) {
+			return sign*MAX_VALUE  ;
+		}
+		// On initialise le tableau des theta_k
+		double tab[] = {0.78539816339744830962,0.099668652491162027378,0.0099996666866652382063,
+			0.00099999966666686666652,0.000099999999666666668667,0.0000099999999996666666667,
+			0.00000099999999999966666667};
+
+		int k = 0 ; //Définit le plus grand angle courant de rotation theta_k possible 
+		float x = (float) 1.0 ; //Abscisse du point courant
+		float xTemp = (float) 1.0 ;
+		float y = (float) 0.0 ; //Ordonnée du point courant
+		float epsilon = power(10,-10) ;
+		
+		while ( theta >= epsilon) {
+			
+			if ( k<6) {
+				while ( theta < tab[k] ) {
+					k++; 
+					if (k>=5){
+						break ;
+					}
+				}
+			}
+			else{
+				while (theta < power(10,-k) ) {
+					k++ ;
+				}
+			}
+			
+			theta = (k<6) ? theta - (float)tab[k] : theta - power(10,-k);
+			xTemp = x ;
+			x = x - power(10,-k)*y ;
+			y = y + power(10,-k)*xTemp;
+		}
+
+		return sign*y/x ;
+	}
+
+	public static float abs(float x){
+		float res = (x>=0)?x:-x;
+		return res;
+	}
+
+	public static float sqrt(float x){
+
+		if (x<0){
+			System.out.println(x+" est un nombre négatif ...");
+			return (float)0.0;
+		}
+		if (x == (float)1.0 || x == (float) 0.0){
+			return x ;
+		}
+		// On multiplie les petits nombres pour en faire des grands nombres.
+		int exp = 0 ;
+		while ( x<1 ) {
+			x = x*power(10,4) ;
+			 // 4 car c'est une puissance de 2 et donc on peut retrouver la racine carré facilement.
+			exp += 4 ;
+		}
+		float low = (float) 0.0; 
+		float high = x;
+		float mid = x;
+		float oldMid = (float) -5.0 ;
+		float epsilon = power(10,-15); 
+		//On fait une recherche dichotomique de la racine ... ce n'est pas niveau coût.
+		while ( abs(oldMid - mid)>= epsilon ) {
+			oldMid = mid ;
+
+			mid = (high + low)/2 ;
+			if (mid*mid > x ){
+				high = mid ;
+			}
+			else{
+				low = mid;
+			}
+		}
+		return mid/power(10,exp/2) ;
+	}
 
 }
--- src/main/resources/include/Test_Math_deca.java
+++ src/main/resources/include/Test_Math_deca.java
@@ -4,12 +4,12 @@ public class Test_Math_deca{
 
 	public static void main(String args[]){
 
-		System.out.println("debut du test pour ulp");
-		System.out.println();
+		System.out.println("debut du test pour ulp\n");
 		int nbErreur = 0 ;
+		int nb = 0 ;		
 		for (float x = new Float(-Math.pow(2,10)) ; x<Math.pow(2,10); x = x + (float)Math.pow(2,-13)){ 
 		// avec un pas de pow(2,-14), il y a un problème pour 1024 + pas = 1024 -> boucle infini
-
+			nb ++ ;
 			if (Math.ulp(x) != Math_deca.ulp(x)) {
 				System.out.println("Erreur pour x="+x);
 				System.out.println("Math.ulp donne : " + Math.ulp(x));
@@ -18,7 +18,47 @@ public class Test_Math_deca{
 				nbErreur ++;
 			}
 		}
-		System.out.println("fin du test pour ulp. Erreur rencontré : " + nbErreur);
+		System.out.println("fin du test pour ulp. Erreur rencontré : " + nbErreur + "sur " +nb+" tests.\n");
+		
 
+		System.out.println("début du test de tan \n");
+		nbErreur = 0 ;
+		nb = 0 ;
+		float diffRelative = (float) 0.0 ;
+		float diff = (float) 0.0 ;
+		for (float x = new Float(-Math.PI/2) ; x<Math.PI/2; x = x + (float)Math.pow(2,-13)){ 
+			nb ++ ;
+			diffRelative = Math.abs(((float)Math_deca.tan(x) - (float)Math.tan(x))/(float)Math.tan(x));
+			diff = diffRelative*(float)Math.tan(x) ;
+			//if ( diff >= 10*Math_deca.ulp((float)Math.tan(x))){ 
+			if ( diffRelative >= Math.pow(10,-3)) {
+				System.out.println("Erreur pour x="+x);
+				// System.out.println("Math_deca.tan : "+Math_deca.tan(x));
+				// System.out.println("Math.tan : "+Math.tan(x));
+				System.out.println("différence relative: " + diffRelative);
+				System.out.println();
+				nbErreur ++;
+			}
+		}		
+		System.out.println("fin du test de tan. Erreur rencontré : " + nbErreur + "sur " +nb+" tests.\n");
+		
+		nbErreur = 0 ;
+		nb = 0 ;
+		System.out.println("début du test de sqrt \n");
+		for(float x = (float) 0 ; x < 100 ; x = x + (float) Math.pow(2,-13)){
+			diffRelative = Math.abs(((float)Math.sqrt(x) - Math_deca.sqrt(x))/(float)Math.sqrt(x));
+			diff = diffRelative*(float)Math.sqrt(x);
+			nb ++ ;
+			//if ( diffRelative >= Math.pow(10,-6)) {
+			if ( diff >= 3*Math_deca.ulp((float)Math.sqrt(x))){
+				System.out.println("Erreur pour x="+x);
+				System.out.println("Math_deca.sqrt : "+Math_deca.sqrt(x));
+				System.out.println("Math.sqrt : "+Math.sqrt(x));
+				System.out.println("différence relative: " + diffRelative);
+				System.out.println();
+				nbErreur ++;
+			}
+		}
+		System.out.println("fin du test de sqrt. Erreur rencontré : " + nbErreur + " sur " +nb+ "tests.\n");
 	}
 }
