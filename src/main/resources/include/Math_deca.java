ecc26e29edfc69ef7d3453bdf251e8bab05876b3
Fischman <fischman.adrien@gmail.com>
2016-01-13 14:54:59 +0100

    fonction upl ne marche pas encore pour les float entre -1 et 1

--- src/main/resources/include/Math_deca.java
+++ src/main/resources/include/Math_deca.java
@@ -3,7 +3,9 @@ import java.lang.Math ;
 public class Math_deca{
 
 	public static float power(float x, int y){
+
 		float originalX = x ;
+
 		if ( y==0 ) {
 			return new Float(1.0);
 		}
@@ -21,11 +23,27 @@ public class Math_deca{
 	}
 
 	public static float ulp(float x){
+
 		int exp = -1 ;
+
 		while (x >= 1 || -x >= 1) {
 			x =  x/(float)2.0;
 			exp ++ ;
 		}
+
+		if(x<= 0) {
+			x = -x ;
+		}	
+
+		while (x  < 0.5) {
+			x = x + (float)0.1 ;
+				exp --  ;
+		}
+
+		// if(exp<-1){
+		// 	exp++;
+		// }
+
 		return power(2,exp - 23) ;		
 	}
 
--- src/main/resources/include/Test_Math_deca.java
+++ src/main/resources/include/Test_Math_deca.java
@@ -4,8 +4,19 @@ public class Test_Math_deca{
 
 	public static void main(String args[]){
 
-		float x = new Float(12.0) ;
-		System.out.println("ulp : "+Math_deca.ulp(x));
-		System.out.println("math ulp " + Math.ulp(x));
+		for (float x = new Float(-Math.pow(2,10)) ; x<=Math.pow(2,10); x = x + (float)Math.pow(2,-5)){
+			//System.out.println(x);
+			if (Math.ulp(x) != Math_deca.ulp(x)) {
+				System.out.println("Erreur pour x="+x);
+				System.out.println("Math.ulp donne : " + Math.ulp(x));
+				System.out.println("Math_deca donne : " + Math_deca.ulp(x));
+				System.out.println();
+			}
+		}
+		// float x = (float) 0.375;
+		// System.out.println("Math.ulp donne : " + Math.ulp(x));
+		// System.out.println("Math_deca donne : " + Math_deca.ulp(x));
+		// System.out.println( Math.pow(2,-26	));
+		
 	}
 }
