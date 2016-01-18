a98e62b3308a237be8889ca4f00617d8dfe83b6b
Matthieu Bergeron <matthieu.bergeron@ensimag.grenoble-inp.fr>
2016-01-18 09:36:10 +0100

    Implémentation readFloat + tests readFloat et readInt

--- src/main/java/fr/ensimag/deca/tree/ReadFloat.java
+++ src/main/java/fr/ensimag/deca/tree/ReadFloat.java
@@ -1,10 +1,7 @@
 package fr.ensimag.deca.tree;
 
-import fr.ensimag.deca.context.Type;
+import fr.ensimag.deca.context.*;
 import fr.ensimag.deca.DecacCompiler;
-import fr.ensimag.deca.context.ClassDefinition;
-import fr.ensimag.deca.context.ContextualError;
-import fr.ensimag.deca.context.EnvironmentExp;
 import fr.ensimag.deca.tools.IndentPrintStream;
 import fr.ensimag.ima.pseudocode.Register;
 import fr.ensimag.ima.pseudocode.instructions.RFLOAT;
@@ -22,7 +19,9 @@ public class ReadFloat extends AbstractReadExpr {
     @Override
     public Type verifyExpr(DecacCompiler compiler, EnvironmentExp localEnv,
             ClassDefinition currentClass) throws ContextualError {
-        throw new UnsupportedOperationException("not yet implemented");
+
+        setType(new FloatType(compiler.getSymbols().create("float")));
+        return this.getType();
     }
 
     @Override
