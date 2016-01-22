188868ceaf8a324ac03c7a31b2bc374b88ed5365
FlorentinGonthier <florentin.gonthier@phema.grenoble-inp.fr>
2016-01-22 17:49:37 +0100

    gestion instanceof

--- src/main/java/fr/ensimag/deca/tree/InstanceOf.java
+++ src/main/java/fr/ensimag/deca/tree/InstanceOf.java
@@ -1,10 +1,12 @@
 package fr.ensimag.deca.tree;
 
 import fr.ensimag.deca.DecacCompiler;
+import fr.ensimag.deca.DecacFatalError;
 import fr.ensimag.deca.context.*;
+import fr.ensimag.deca.tools.DecacInternalError;
 import fr.ensimag.deca.tools.IndentPrintStream;
-import fr.ensimag.ima.pseudocode.DVal;
-import fr.ensimag.ima.pseudocode.GPRegister;
+import fr.ensimag.ima.pseudocode.*;
+import fr.ensimag.ima.pseudocode.instructions.*;
 import org.apache.commons.lang.Validate;
 
 import java.io.PrintStream;
@@ -35,6 +37,30 @@ public class InstanceOf extends AbstractExpr {
 
     @Override
     public void codegenExpr(DecacCompiler compiler, GPRegister register) {
+        if(var.getDval()==null){
+            throw new DecacInternalError("element vide");
+        }
+        else{
+            boolean[] backup =compiler.getRegManager().getTableRegistre();
+            GPRegister bypass = compiler.getRegManager().getGBRegister();
+            GPRegister stock = compiler.getRegManager().getGBRegister();
+            int i=compiler.getLblManager().getIf();
+            compiler.getLblManager().incrementIf();
+            compiler.addInstruction(new LOAD(var.getDval(),bypass));
+            compiler.addInstruction(new LOAD(className.getClassDefinition().getOperand(),stock));
+            compiler.addLabel(new Label("debut.instanceof"+i));
+            compiler.addInstruction(new CMP(bypass,stock));
+            compiler.addInstruction(new BEQ(new Label("true.instanceof."+i))); //test si egal
+            compiler.addInstruction(new LOAD(new RegisterOffset(0,bypass),bypass)); // on descend
+            compiler.addInstruction(new CMP(new NullOperand(),bypass)); //si object instance
+            compiler.addInstruction(new BNE(new Label("debut.instanceof"+i))); //non, on remonte
+            compiler.addInstruction(new LOAD(new ImmediateInteger(0),register));
+            compiler.addInstruction(new BRA(new Label("fin.instanceof"+i)));
+            compiler.addLabel(new Label("true.instanceof."+i));
+            compiler.addInstruction(new LOAD(new ImmediateInteger(1),register));
+            compiler.addLabel(new Label("fin.instanceof"+i));
+            compiler.getRegManager().setTableRegistre(backup);
+        }
 
     }
 
