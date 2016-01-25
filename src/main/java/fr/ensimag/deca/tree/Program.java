2a939dde7abe103a6fa0ef408373d6012825a16f
Matthias Beaupère <matthias.beaupere@ensimag.grenoble-inp.fr>
2016-01-25 17:08:01 +0100

    retrait d'un println

--- src/main/java/fr/ensimag/deca/tree/Program.java
+++ src/main/java/fr/ensimag/deca/tree/Program.java
@@ -40,7 +40,6 @@ public class Program extends AbstractProgram {
     @Override
     public void verifyProgram(DecacCompiler compiler) throws ContextualError {
         LOG.debug("verify program: start");
-        System.out.println("ok");
 
         EnvironmentTypes envTypes = new EnvironmentTypes(null);
         try {
